337cdbbcea6d84b9329933cef1ceb89ac7d342ec
Nandita99-code <74102644+Nandita99-code@users.noreply.github.com>
2020-12-01 17:29:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var myEngine, myWorld;
+var box1;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    myEngine = Engine.create();
+    myWorld = myEngine.world;
+
+    box1= new Box(200,300,50,50);
+    
+
+    
+   
+
+   
+    
+}
+
+function draw(){
+    background(0);
+    Engine.update(myEngine);
+   box1.display();
+  
+}
